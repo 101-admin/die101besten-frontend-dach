9f2a3c6062404cbd50391bc44aa0ac2b53068b41
laibaiqbal552 <laibaiqbal552@gmail.com>
2026-03-07 03:10:39 +0500

    links updated

--- src/Data/Edition.ts
+++ src/Data/Edition.ts
@@ -2,19 +2,18 @@ const Edition = [
   {
     title: "Edition Deutschland",
     img: "/images/Edition-2.png",
-    link:'https://die-new.vercel.app/'
+    link: "https://die101besten-frontend-de.vercel.app/",
   },
   {
     title: "Edition DACH + Südtirol",
     img: "/images/Edition-1.png",
-    link:'https://die101besten-frontend-dach-steel.vercel.app/'
+    link: "https://die101besten-frontend-dach.vercel.app/",
   },
   {
     title: "Edition Schweiz",
     img: "/images/Edition-3.png",
-    link:'https://die101besten-frontend-ch-eight.vercel.app/'
+    link: "https://die101besten-frontend-ch.vercel.app/",
   },
 ];
 
 export default Edition;
-  
